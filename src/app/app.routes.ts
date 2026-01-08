c2cb24a8cc7c9097db7bf4c88aa105ead1ce074e
Pachara Tinamas <mrpachara@gmail.com>
2026-01-08 22:28:05 +0700

    finish eample

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: 'example', pathMatch: 'full' },
+  { path: 'example', loadChildren: () => import('./example/routes') },
+];
